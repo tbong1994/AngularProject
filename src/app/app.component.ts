0ae147254204cba51a955f22fbd671407dfa13e0
TB <youngkeu@buffalo.edu>
2017-08-06 18:43:15 -0400

    Added an image for the main page, created the title of the application, defined a character object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Harry Potter';
+  character : Character = {
+    name : 'Harry Potter',
+    dob : new Date(7/31/1980),
+    address: "NO.4 Privet Dr, London",
+    school: "Hogwartz"
+  };
+}
+
+export class Character {
+  name : string;
+  dob : Date;
+  address: string;
+  school: string;
 }
