65638fce10099400a0a234e8bc99f9fe3c29abba
Thomas Bong <tbong1994@gmail.com>
2017-08-15 08:08:21 -0400

    implemented onSelect function in dashboard component

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Wizard } from './wizard.component/wizard';
 import { WizardService } from './wizard.component/wizard.service';
-
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Location }                 from '@angular/common';
 
 //DASHBOARD ONLY DISPLAYS THE TOP 3 CHARACTERS
 @Component({
@@ -13,11 +14,21 @@ import { WizardService } from './wizard.component/wizard.service';
 
 export class DashboardComponent implements OnInit { 
     public wizards : Wizard[] = [];
+    private selectedWizard : Wizard;
+    private location : Location;
     
-    constructor(private wizService: WizardService) {
+    constructor(private wizService: WizardService, private router: Router) {
     }
     ngOnInit(){
         this.wizService.getWizards().then( wizards => this.wizards = wizards.slice(0,3) );
     }
-
+    goBack(): void {
+    this.location.back();
+  }
+    onSelect( wizard: Wizard){
+        this.selectedWizard = wizard;
+    }
+    goToDetail(): void{
+        this.router.navigate(['/wizard', this.selectedWizard.name]);
+    }
 }
